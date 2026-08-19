006e0f7c607b6112f56504f238e040fdaba53131
mamantube <firmanhiday.id007@gmail.com>
2026-08-19 15:43:02 +0700

    try code

--- src/controllers/transaction/notification.post.js
+++ src/controllers/transaction/notification.post.js
@@ -77,21 +77,21 @@ export default async function (req, res) {
       };
     } else if (paymentType === "qris") {
       console.log("========== QRIS DEBUG ==========");
-      console.log("payment_type:", transactionStatus.payment_type);
-      console.log("qr_string:", transactionStatus.qr_string);
-      console.log("actions:", transactionStatus.actions);
-      console.log("================================");
-      
-      const qrAction = transactionStatus.actions?.find(
-        (action) =>
-          action.name === "generate-qr-code-v2" ||
-          action.name === "generate-qr-code",
-      );
+    console.log("payment_type:", transactionStatus.payment_type);
+    console.log("qr_string:", transactionStatus.qr_string);
+    console.log("actions:", transactionStatus.actions);
+    console.log("================================");
 
-      updateData.payment_detail = {
-        qr_url: qrAction?.qr_url || null,
+    const qrAction = transactionStatus.actions?.find(
+        (action) =>
+            action.name === "generate-qr-code-v2" ||
+            action.name === "generate-qr-code"
+    );
+
+    updateData.payment_detail = {
+        qr_url: qrAction?.url || null,
         qr_string: transactionStatus.qr_string || null,
-      };
+    };
 
       console.log("== QR PAYMENT DETAIL ==");
       console.log(updateData.payment_detail);
