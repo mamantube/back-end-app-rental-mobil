b66579386411a898840b6233fcd6905c35281abb
mamantube <firmanhiday.id007@gmail.com>
2026-08-19 14:58:55 +0700

    try to get qr url or qr string

--- src/controllers/transaction/notification.post.js
+++ src/controllers/transaction/notification.post.js
@@ -14,19 +14,31 @@ export default async function (req, res) {
     console.log("=== MIDTRANS NOTIFICATION ===");
     console.log(notification);
 
-    const { order_id, transaction_status, payment_type, transaction_id, gross_amount, status_code, signature_key, va_numbers } = notification;
+    const {
+      order_id,
+      transaction_status,
+      payment_type,
+      transaction_id,
+      gross_amount,
+      status_code,
+      signature_key,
+      va_numbers,
+    } = notification;
 
     if (!order_id) {
-        console.error("Order ID tidak ditemukan")
+      console.error("Order ID tidak ditemukan");
       return message(res, 200, "Order ID tidak ditemukan");
     }
 
-    const signature = crypto.createHash("sha512").update( order_id + status_code + gross_amount + MIDTRANS_SERVER_KEY).digest("hex");
+    const signature = crypto
+      .createHash("sha512")
+      .update(order_id + status_code + gross_amount + MIDTRANS_SERVER_KEY)
+      .digest("hex");
 
     if (signature !== signature_key) {
-        console.error("Signature midtrans tidak valid");
+      console.error("Signature midtrans tidak valid");
 
-        return message(res, 403, "Invalid signature");
+      return message(res, 403, "Invalid signature");
     }
 
     const core = new midtransClient.CoreApi({
@@ -44,59 +56,73 @@ export default async function (req, res) {
 
     const updateData = {
       status: transactionStatus.transaction_status || transaction_status,
-      transaction_id: transactionStatus.transaction_id || transaction_id || null,
+      transaction_id:
+        transactionStatus.transaction_id || transaction_id || null,
       payment_type: paymentType,
     };
 
-    if ( paymentType === "bank_transfer" && va_numbers && va_numbers.length > 0 ) {
-        const va = transactionStatus.va_numbers[0];
+    if (
+      paymentType === "bank_transfer" &&
+      va_numbers &&
+      va_numbers.length > 0
+    ) {
+      const va = transactionStatus.va_numbers[0];
 
-        console.log("== VA DATA ==");
-        console.log(va);
+      console.log("== VA DATA ==");
+      console.log(va);
 
-        updateData.payment_detail = {
-            bank: va.bank,
-            va_number: va.va_number,
-        }
-    } else if ( paymentType === "qris" ) {
-        const qrAction = transactionStatus.actions?.find((action) => action.name === "generate-qr-code-v2" || action.name === "generate-qr-code")
+      updateData.payment_detail = {
+        bank: va.bank,
+        va_number: va.va_number,
+      };
+    } else if (paymentType === "qris") {
+      const qrAction = transactionStatus.actions?.find(
+        (action) =>
+          action.name === "generate-qr-code-v2" ||
+          action.name === "generate-qr-code",
+      );
 
-        console.log("== QR ACTION ==");
-        console.log(qrAction);
+      updateData.payment_detail = {
+        qr_url: qrAction?.url || null,
+        qr_string: transactionStatus.qr_string || null,
+      };
 
-        updateData.payment_detail = {
-            qr_url: qrAction?.url ||null,
-        };
+      console.log("== QR PAYMENT DETAIL ==");
+      console.log(updateData.payment_detail);
     } else {
-        updateData.payment_detail = {
-            transaction_id: transactionStatus.transaction_id || transaction_id || null,
-        };
+      updateData.payment_detail = {
+        transaction_id:
+          transactionStatus.transaction_id || transaction_id || null,
+      };
     }
 
     console.log("== UPDATE DATA ==");
     console.log(updateData);
 
     const transaction = await transactionModel.findOneAndUpdate(
-        {
-            order_id: order_id,
-        },
-        updateData,
-        {
-            new: true,
-        }
+      {
+        order_id: order_id,
+      },
+      updateData,
+      {
+        new: true,
+      },
     );
 
     if (!transaction) {
-        console.warn(`Transaksi ${order_id} tidak ditemukan di database`);
+      console.warn(`Transaksi ${order_id} tidak ditemukan di database`);
 
-        return message(res, 200, "Notifikasi diterima, transaksi tidak ditemukan")
+      return message(
+        res,
+        200,
+        "Notifikasi diterima, transaksi tidak ditemukan",
+      );
     }
 
     console.log("== TRANSACTION UDPATE ==");
     console.log(transaction);
 
-    return message(res, 200, "Notifikasi berhasil diproses", transaction)
-    
+    return message(res, 200, "Notifikasi berhasil diproses", transaction);
   } catch (error) {
     console.error("MIDTRANS NOTIFICATION ERROR", error);
 
