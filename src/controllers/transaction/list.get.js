73ccbc3d02e17f8076851e79e0e2408b32b7bdcb
mamantube <firmanhiday.id007@gmail.com>
2026-08-22 11:07:16 +0700

    update code

--- src/controllers/transaction/list.get.js
+++ src/controllers/transaction/list.get.js
@@ -37,7 +37,7 @@ export default async function (req, res) {
 
     const q = req.query.q || "";
     const page = req.query.page ? Number(req.query.page) : 1;
-    const per_page = req.query.page ? Number(req.query.per_page) : 10;
+    const per_page = req.query.per_page ? Number(req.query.per_page) : 10;
     const skip = page > 1 ? (page - 1) * per_page : 0;
 
     const macthQuery = {
